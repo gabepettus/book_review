7837e5e9872911a01f3dd53c7c89c3b591b7f835
gabe pettus <gpettus@icr-team.com>
2021-06-15 22:40:48 -0600

    added stubs for routes, landing page, and errors

---
+++ resources/views/common/errors.blade.php
@@ -0,0 +1,17 @@
+{{-- resources/views/common/errors.blade.php --}}
+{{-- A single consolidate error page for now
+    TODO - clean up page --}}
+@if (count($errors) > 0)
+    <!-- Form Error List -->
+    <div class="alert alert-danger">
+        <strong>Whoops! Something went wrong!</strong>
+
+        <br><br>
+
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Models\Book;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +18,41 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $books = Book::orderBy('author','asc')->get();
+    return view('books/booklist', [
+        'books' => $books
+    ]);
 });
 
 Route::get('/books', [BookController::class, 'index']);
+
+/**
+ * Add A New Book
+ */
+Route::post('/Book', function (Request $request) {
+
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:5',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $book = new Book;
+    $book->name = $request->name;
+    $book->save();
+
+    return redirect('/');
+});
+
+/**
+ * Delete An Existing Book
+ */
+Route::delete('/book/{book}', function (Book $book) {
+    $book->delete();
+
+    return redirect('/');
+});
