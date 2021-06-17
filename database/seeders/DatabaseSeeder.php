c43375781b4687cbe2cc8ae6a5cadbc66040fef1
gabe pettus <gpettus@icr-team.com>
2021-06-17 11:09:43 -0600

    added hardcoded seeder and stubs for factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +18,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+            DB::table('books')->insert([
+                'id' => '1',
+                'name' => 'James and the Giant Peach',
+                'author' => 'Roald Dahl',
+                'date_published' => '1961-07-17',
+                'photo' => 'https://en.wikipedia.org/wiki/James_and_the_Giant_Peach#/media/File:JamesAndTheGiantPeach.jpg'
+            ]);
+        
+            DB::table('books')->insert([
+                'id' => '2',
+                'name' => 'Zen and the Art of Motorcycle Maintenance: An Inquiry into Values',
+                'author' => 'Robert M. Pirsig',
+                'date_published' => '1974-04-01',
+                'photo' => 'https://en.wikipedia.org/wiki/Zen_and_the_Art_of_Motorcycle_Maintenance#/media/File:Zen_motorcycle.jpg'
+            ]);
     }
 }
